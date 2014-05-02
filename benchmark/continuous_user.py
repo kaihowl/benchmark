5d7f7b8c7ff94dfa9877f275258a6ec8ef28ddb2
kaihowl <kai@hoewelmeyer.eu>
2014-05-02 14:57:11 +0200

    Continuous user formats query with userArgs

--- benchmark/continuous_user.py
+++ benchmark/continuous_user.py
@@ -5,7 +5,7 @@ class ContinuousUser(User):
     def runUser(self):
         cur_query = self._totalRuns % len(self._queries)
         query_name = self._queries[cur_query]
-        query = self._queryDict[query_name]
+        query = self._queryDict[query_name] % self._userArgs
         result = self.fireQuery(query).json()
         perf = result.get("performanceData", None)
         self.log("queries", [query_name,  perf])
