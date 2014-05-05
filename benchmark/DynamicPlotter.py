a3d285773fd48c8dfa17c49185fa59c3b16d16c5
kaihowl <kai@hoewelmeyer.eu>
2014-05-05 14:47:12 +0200

    Output plot file name for notify script

--- benchmark/DynamicPlotter.py
+++ benchmark/DynamicPlotter.py
@@ -78,6 +78,8 @@ class DynamicPlotter:
 
       fname = "varying_mts_%s.pdf" % str(int(time.time()))
       plt.savefig(fname)
+      # Output filename for notify to attach to email
+      print "\n<<<%s\n" % fname
       plt.close()
 
     # returns a dictionary
