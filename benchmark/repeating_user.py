0c9ccc020c88bf1742b4d25d34931460407fa470
kaihowl <kai@hoewelmeyer.eu>
2014-05-26 12:08:48 +0200

    Added repeating user

---
+++ benchmark/repeating_user.py
@@ -0,0 +1,23 @@
+from user import User
+
+# Repeats queries for a fixed number of runs
+# The number is supplied in userArgs["repetitions"]
+class RepeatingUser(User):
+
+    def __init__(self, userId, host, port, dirOutput, queryDict, **kwargs):
+        User.__init__(self, userId, host, port, dirOutput, queryDict, **kwargs)
+        self._repetitions = kwargs["repetitions"] if kwargs.has_key("repetitions") else 1
+
+    def runUser(self):
+        if self._totalRuns >= self._repetitions:
+            self._stopevent.set()
+            return
+        cur_query = self._totalRuns % len(self._queries)
+        query_name = self._queries[cur_query]
+        query = self._queryDict[query_name] % self._userArgs
+        result = self.fireQuery(query).json()
+        perf = result.get("performanceData", None)
+        self.log("queries", [query_name,  perf])
+
+    def formatLog(self, key, value):
+        return "%s;%s\n" % (value[0], value[1])
